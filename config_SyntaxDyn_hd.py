c1495f6656e217f32ee2fb2e271332da002f4b45
Dee Bolger <deirdre.bolger76@gmail.com>
2022-10-13 10:17:56 +0200

    Adapted the script so that data is saved in BIDS-compatible manner. - Creation of a directory for each participant. EEG data is saved in an "eeg" directory in the current participant's directory.

--- config_SyntaxDyn_hd.py
+++ config_SyntaxDyn_hd.py
@@ -27,10 +27,12 @@ import os
 import mne
 import mne_bids
 from mne_bids import BIDSPath, read_raw_bids
+import json
 
 study_name = 'DynSyn_EEG_PC'
-bids_root  = '/Users/bolger/Documents/work/Projects/Project_SyntaxDyn_EGI/SyntaxDyn_hd'    # Root directory
-deriv_root = '/Users/bolger/Documents/work/Projects/Project_SyntaxDyn_EGI/SyntaxDyn_hd/derivatives'
+study_root = '/Users/bolger/Documents/work/Projects/Project_DynSyn_EEG_PC'
+bids_root  =  '/'.join([study_root, study_name])    # Root directory
+deriv_root = '/'.join([bids_root, 'derivatives'])
 
 sessions = ['1']
 subjects = ['05']
@@ -38,7 +40,6 @@ ch_types = ['eeg']
 data_type = 'eeg'
 
 interactive = False
-
 resample_sfreq = 250
 eeg_template_montage = mne.channels.make_standard_montage('GSN-HydroCel-257')
 
@@ -70,7 +71,17 @@ plot_psd_for_runs = []
 N_JOBS = 2
 parallel_backend = 'loky'
 
-## Define the minimum/maximum epoch time
+## READ IN THE DICTIONAY OF EVENT INFORMATION STORED AS A JSON FILE.
+#  We load in the events_dict.son file of the first subject in the list as the
+#  events_dict.json is the same for each subject.
+#  The events_dict.json file is stored at the level of 'ses-1' directory.
+
+trigcorr_fpath = '/'.join([bids_root, 'sub-'+subjects[0], 'ses-1'])
+onsetfile = [f for f in os.listdir(trigcorr_fpath) if f.endswith('dict.json')]
+with open('/'.join([trigcorr_fpath, onsetfile[0]])) as fonsets:
+    onsetcorr_data = fonsets.read()
+onsetcorr_js = json.loads(onsetcorr_data)   # Reconstruct onsetcorr_data as a dictionary.
+
 epochs_tim = -0.25
 epochs_tmax = 1.000
 baseline = (None, 0)
